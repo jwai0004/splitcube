e79d19a74f1cdcba2c1d50a8cf4deb9639f049c5
Jackson Wain <jwai0004@student.monash.edu>
2019-04-23 19:17:58 +1000

    Added name sanitisation for filenames

--- scripts/firstPrinting.py
+++ scripts/firstPrinting.py
@@ -1,14 +1,19 @@
 import json, os, requests
-
+from pathvalidate import sanitize_filename
 
 def get_first_printings():
-    with open('../bulkData/scryfall-default-cards.json', 'r') as f:
+
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../bulkData/scryfall-default-cards.json')
+
+    with open(filename, 'r') as f:
         file_json = json.load(f)
 
     first_printings = []
 
     for card in file_json:
-        if not card['reprint']:
+        # Process only if first printing from main set
+        if not card['reprint'] and len(card['set']) == 3:
             try:
                 card_dict = {
                     'name': card['name'],
@@ -39,12 +44,18 @@ def save_image_file(image_url, name):
     r = requests.get(image_url)
 
     dirname = os.path.dirname(__file__)
-    filename = '../images/' + str(name)
+    filename = '../images/' + sanitize_filename(name) + '.jpg'
     filepath = os.path.join(dirname, filename)
 
     with open(filepath, 'wb') as f:
         f.write(r.content)
 
 if __name__ == '__main__':
+    import random, time
     card_list = get_first_printings()
-    save_image_file(card_list[0]['image'], card_list[0]['name'])
+    for _ in range(10):
+        ind = random.randint(0, len(card_list))
+        print(card_list[ind]['name'])
+        save_image_file(card_list[ind]['image'], card_list[ind]['name'])
+        # Sleep for 100ms for around 10 requests per second
+        time.sleep(0.1)
